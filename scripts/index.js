f3755f34f21ae0b9f187e0369bf8abf074bc4371
André <andrejr.mello@hotmail.com>
2022-12-27 18:02:57 -0300

    implementacao querys para mobile
    Implementação de querys para mobile, e  criação do layout do nav-menu button 'hamburguer'

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 // Cursor-mouse
 const cursor = document.getElementById('cursor-mouse')
 
-window.addEventListener('mousemove', function(e) {
-    cursor.style.top = e.pageY - 20 + "px"
-    cursor.style.left = e.pageX - 20 + "px"
-    cursor.style.display = 'block'
-})
-
-// window.addEventListener('scroll', () => {cursor.style.display = 'none'} )
+if(window.screen.width >= 992){
+    window.addEventListener('mousemove', function(e) {
+        cursor.style.top = e.pageY - 20 + "px"
+        cursor.style.left = e.pageX - 20 + "px"
+        cursor.style.display = 'block'
+        
+    })
+}
