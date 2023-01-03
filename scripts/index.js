28e5b008c69ef2eaec2b1bc6ab6ac01595db7085
André <andrejr.mello@hotmail.com>
2023-01-03 01:16:12 -0300

    ul itens efeito digitacao [mobile]
    Adicionado efeito digitação na ul da navbar, apenas no mobile.

--- scripts/index.js
+++ scripts/index.js
@@ -9,36 +9,3 @@ if(window.screen.width >= 992){
         cursor.style.display = 'block'   
     })
 }
-
-// botão navbar mobile
-let menuIconEl = document.querySelector('#icon-menu')
-let ulNavEl = document.querySelector('header nav#navbar ul')
-let iconIconEl = document.querySelector('.icon-icon-menu')
-
-let menuIcon = false
-
-if(window.screen.width <= 768) {
-    menuIconEl.addEventListener('click', function(e) {
-        
-        if(menuIcon) {
-            menuIcon = !menuIcon
-            ulNavEl.style.opacity = 0
-            ulNavEl.style.pointerEvents = 'none'
-            iconIconEl.setAttribute('src', 'assets/images/hamburguer.png')
-        } else {
-            menuIcon = !menuIcon
-            ulNavEl.style.opacity = 1
-            ulNavEl.style.pointerEvents = 'all'
-            iconIconEl.setAttribute('src', 'assets/images/icon-x.png')
-        }
-
-
-        // menuIcon ? menuIcon = false : menuIcon = true
-        // menuIcon ? 
-        //     ulNavEl.style.opacity = 1: 
-        //     ulNavEl.style.opacity = 0
-        // menuIcon ? 
-        //     iconIconEl.setAttribute('src', 'assets/images/icon-x.png') :
-        //     iconIconEl.setAttribute('src', 'assets/images/hamburguer.png') 
-    })
-}
---
+++ scripts/navbar.js
@@ -0,0 +1,59 @@
+////// Códigos para efeito no botão navbar mobile
+if(window.screen.width <= 768) {
+
+const menuIconEl = document.querySelector('#icon-menu')
+const itens = document.querySelectorAll('.ulNav li a')
+const iconIconEl = document.querySelector('.icon-icon-menu')
+
+let menuIcon = false
+
+let lista = []
+
+itens.forEach(item => {
+    lista.push(item.textContent)
+    item.textContent = ''
+})
+
+/// TIMER
+function timer(ms) {
+    return new Promise( resolve => {
+        setTimeout(()=> {resolve('')}, ms)
+    })
+}
+
+// efeito digitar
+async function typing(clicked) {
+    if(clicked) {
+        for(let i = 0; i < itens.length; i++) {
+            itens[i].style.display = 'inline-block'
+            for(let b = 0; b < lista[i].length; b++) {
+            await timer(40)
+            itens[i].textContent += lista[i].charAt(b)
+            }
+        }
+    } else {
+        for(let i = 0; i < itens.length; i++) {
+            var itemList = itens[i].textContent.split('')
+            for(let b = 0; b < lista[i].length; b++) {
+                await timer(40)   
+                itemList.pop()
+                itens[i].textContent = itemList.join('')
+            }
+            itens[i].style.display = 'none'
+        } 
+    }
+}
+
+// click botão haburg
+    menuIconEl.addEventListener('click', () => {
+        if(menuIcon) {
+            menuIcon = !menuIcon
+            typing(false)
+            iconIconEl.setAttribute('src', 'assets/images/hamburguer.png')
+        } else {
+            menuIcon = !menuIcon
+            typing(true)
+            iconIconEl.setAttribute('src', 'assets/images/icon-x.png')
+        }
+    })
+} // fecha if linha 2
