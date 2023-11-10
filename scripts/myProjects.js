ce23adc20319fa8f107829c5746423e471afe45b
André Jr <andrejr.mello@hotmail.com>
2023-11-09 22:56:37 -0300

    novo my projects finalizado

--- scripts/myProjects.js
+++ scripts/myProjects.js
@@ -1,13 +1,35 @@
-let firstBtn = document.querySelector(".folder-btn.first");
-let secBtn = document.querySelector(".folder-btn.sec");
-
-
-
-firstBtn.addEventListener("click", toggleFolder);
-secBtn.addEventListener("click", toggleFolder);
+let firstBtn;
+let secBtn;
 
 function toggleFolder () {
     console.log("click");
     firstBtn.classList.toggle("openFolder");
     secBtn.classList.toggle("openFolder");
+    
+    if (firstBtn.classList.contains("openFolder")) {
+        carregarProjetos('backend');
+    }
+    else {
+        carregarProjetos('frontend');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    carregarProjetos('frontend');
+});
+
+function carregarProjetos( arquivo ) {
+    // Carrega o conteúdo do outro arquivo HTML
+    fetch(`../styles/components/myProjects/${arquivo}.html`)
+    .then(response => response.text())
+    .then(data => {
+        // Substitui o conteúdo da div pelo conteúdo do arquivo carregado
+        document.getElementById('dynamic-js').innerHTML = data;
+        // Pega os botões de troca de conteúdo
+        firstBtn = document.querySelector(".folder-btn.first");
+        secBtn = document.querySelector(".folder-btn.sec");
+        firstBtn.addEventListener("click", toggleFolder);
+        secBtn.addEventListener("click", toggleFolder);
+    })
+    .catch(error => console.error('Erro ao carregar o arquivo:', error));
 }
